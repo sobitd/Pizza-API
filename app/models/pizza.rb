36f7d17003a1d36b8367e9554f7b5413c7a00991
Daniel Sobit <sobitdaniel@gmail.com>
2022-10-02 13:53:10 +0300

    create many-to-many relationship

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,3 @@
 class Pizza < ApplicationRecord
+  has_many :restaurants, through: :restaurant_pizzas
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+  has_many :pizzas, through: :restaurant_pizzas
 end
