f5f81f6f1ca10796a493fd175cf9d07b097a5a98
Daniel Sobit <sobitdaniel@gmail.com>
2022-10-02 13:48:47 +0300

    create join table restaurant_pizza

---
+++ db/migrate/20221002104642_create_restaurant_pizzas.rb
@@ -0,0 +1,11 @@
+class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurant_pizzas do |t|
+      t.integer :price
+      t.references :restaurant, null: false, foreign_key: true
+      t.references :pizza, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_154254) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_02_104642) do
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurant_pizzas", force: :cascade do |t|
+    t.integer "price"
+    t.integer "restaurant_id", null: false
+    t.integer "pizza_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pizza_id"], name: "index_restaurant_pizzas_on_pizza_id"
+    t.index ["restaurant_id"], name: "index_restaurant_pizzas_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -25,4 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_154254) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "restaurant_pizzas", "pizzas"
+  add_foreign_key "restaurant_pizzas", "restaurants"
 end
