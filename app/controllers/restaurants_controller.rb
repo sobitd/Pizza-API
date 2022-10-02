3a205e21a40e51fe083b28d8ba83a5cfc3e35c35
Daniel Sobit <sobitdaniel@gmail.com>
2022-10-02 20:33:56 +0300

    restaurant routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,29 @@
 class RestaurantsController < ApplicationController
   def index
     restaurants = Restaurant.all
-    render json: restaurants, only: [:id, :name, :address]
+    render json: restaurants, only: %i[id name address]
   end
 
   def show
     restaurant = Restaurant.find(params[:id])
-    render json: {
-      id: restaurant.id,
-      name: restaurant.name,
-      address: restaurant.address
-    }
+    if restaurant
+      render json: {
+        id: restaurant.id,
+        name: restaurant.name,
+        address: restaurant.address
+      }
+    else
+      render json: { error: 'Restaurant not found' }, status: :not_found
+    end
+  end
+
+  def destroy
+    restaurant = Restaurant.find(params[:id])
+    if restaurant
+      restaurant.destroy
+      head :no_content
+    else
+      render json: { error: 'Restaurant not found' }, status: :not_found
+    end
   end
 end
