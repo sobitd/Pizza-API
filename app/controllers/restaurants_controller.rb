0de329f191fc4b8054ca7d2240f362c9fcfec6d1
Daniel Sobit <sobitdaniel@gmail.com>
2022-10-02 16:04:03 +0300

    remove some content when rendering

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,15 @@
 class RestaurantsController < ApplicationController
   def index
     restaurants = Restaurant.all
-    render json: restaurants
+    render json: restaurants, only: [:id, :name, :address]
+  end
+
+  def show
+    restaurant = Restaurant.find(params[:id])
+    render json: {
+      id: restaurant.id,
+      name: restaurant.name,
+      address: restaurant.address
+    }
   end
 end
