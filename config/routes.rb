dac67adc01dd04e9b87c78661d0734a66baee2b6
Daniel Sobit <sobitdaniel@gmail.com>
2022-10-02 20:38:28 +0300

    add pizzas controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: %i[index show destroy]
+  resources :pizzas, only: [:index]
 end
