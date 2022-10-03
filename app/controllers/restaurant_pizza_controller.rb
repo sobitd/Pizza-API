10f3f13a5442648fb86b54e4b72801543d5bb2ad
Daniel Sobit <sobitdaniel@gmail.com>
2022-10-03 08:28:37 +0300

    add numericality validation to prices

--- app/controllers/restaurant_pizza_controller.rb
+++ app/controllers/restaurant_pizza_controller.rb
@@ -1,3 +1,6 @@
 class RestaurantPizzaController < ApplicationController
-    
+  def create
+    weekday_special = RestaurantPizza.create(params.permit(:price, :pizza_id, :restaurant_id))
+    render json: weekday_special, status: :created
+  end
 end
