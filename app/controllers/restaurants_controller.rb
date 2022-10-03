5e0ad5a3c3498e0ddd1375f86c7d521e37194384
Daniel Sobit <sobitdaniel@gmail.com>
2022-10-03 08:51:40 +0300

    delete attribute filter only on restaurants in controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
     restaurants = Restaurant.all
-    render json: restaurants, only: %i[id name address]
+    render json: restaurants
   end
 
   def show
